43654e376e044e061d369f549fe36a9d048976e1
Ananiev_IM <ilya.anunev@gmail.com>
2024-11-01 23:30:34 +0700

    'reformat code'

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -2,12 +2,12 @@ public class PosterManager {
     private String[] moviesList = new String[0];
 
     public void add(String movie) {
-        String[] tmp = new String[moviesList.length+1];
+        String[] tmp = new String[moviesList.length + 1];
         for (int i = 0; i < moviesList.length; i++) {
             tmp[i] = moviesList[i];
         }
-        tmp[tmp.length-1]=movie;
-        moviesList=tmp;
+        tmp[tmp.length - 1] = movie;
+        moviesList = tmp;
     }
 
     public String[] showAll() {
@@ -18,13 +18,12 @@ public class PosterManager {
         int counter = 0;
         if (moviesList.length < 5) {
             counter = moviesList.length;
-        }
-        else {
+        } else {
             counter = 5;
         }
         String[] tmp = new String[counter];
         for (int i = 0; i < counter; i++) {
-            tmp[i] = moviesList[moviesList.length-1-i];
+            tmp[i] = moviesList[moviesList.length - 1 - i];
         }
         return tmp;
     }
@@ -33,13 +32,12 @@ public class PosterManager {
         int counter = 0;
         if (moviesList.length < howManyMovies) {
             counter = moviesList.length;
-        }
-        else {
+        } else {
             counter = howManyMovies;
         }
         String[] tmp = new String[counter];
         for (int i = 0; i < counter; i++) {
-            tmp[i] = moviesList[moviesList.length-1-i];
+            tmp[i] = moviesList[moviesList.length - 1 - i];
         }
         return tmp;
     }
